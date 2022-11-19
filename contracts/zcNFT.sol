dd87754e33b62fd3c75981790e676bd1d3b072cc
mnscript <mn.nekhoda@gmail.com>
2022-11-18 23:23:42 -0600

    Stripping down basic contract

---
+++ contracts/zcNFT.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Base64.sol";
+
+contract zcNFT is ERC721Enumerable, Ownable { //NFT name specified here "zcNFT"
+  using Strings for uint256;
+
+  constructor() ERC721("Zero Contrast On Chain NFT", "ZCN") {}
+
+  // public
+  function mint(uint256 _mintAmount) public payable {
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0);
+    require(supply + _mintAmount <= 20); // change max supply (20)
+
+    if (msg.sender != owner()) {
+      require(msg.value >= 0.05 ether * _mintAmount); // change cost of NFT (0.05)
+    }
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+  }
+ 
+  //only owner
+  function withdraw() public payable onlyOwner {
+    // This will pay HashLips 5% of the initial sale.
+    // You can remove this if you want, or keep it in to support HashLips and his channel.
+    // =============================================================================
+    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
+    require(hs);
+    // =============================================================================
+    
+    // This will payout the owner 95% of the contract balance.
+    // Do not remove this otherwise you will not be able to withdraw the funds.
+    // =============================================================================
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+    // =============================================================================
+  }
+}
