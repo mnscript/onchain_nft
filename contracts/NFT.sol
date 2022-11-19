dbc502cd0b9bf4e5f3a9c3c06409e8df5acacb74
mnscript <mn.nekhoda@gmail.com>
2022-11-18 22:59:37 -0600

    Resetting initial NFT contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,90 +1,124 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Base64.sol";
 
-contract PrettyAwesomeWords is ERC721Enumerable, Ownable {
+contract NFT is ERC721Enumerable, Ownable {
   using Strings for uint256;
-  
-  string[] public wordsValues = ["accomplish","accepted","absolutely","admire","achievement","active","adorable","affirmative","appealing","approve","amazing","awesome","beautiful","believe","beneficial","bliss","brave","brilliant","calm","celebrated","champion","charming","congratulation","cool","courageous","creative","dazzling","delightful","divine","effortless","electrifying","elegant","enchanting","energetic","enthusiastic","excellent","exciting","exquisite","fabulous","fantastic","fine","fortunate","friendly","fun","funny","generous","giving","great","happy","harmonious","healthy","heavenly","honest","honorable","impressive","independent","innovative","intelligent","intuitive","kind","knowledgeable","legendary","lucky","lovely","marvelous","motivating","nice","perfect","phenomenal","popular","positive","productive","refreshing","remarkable","skillful","sparkling","stunning","successful","supporting","terrific","tranquil","trusting","vibrant","wholesome","worthy","wonderful"];
-  
-   struct Word { 
-      string name;
-      string description;
-      string bgHue;
-      string textHue;
-      string value;
-   }
-  
-  mapping (uint256 => Word) public words;
-  
-  constructor() ERC721("Pretty Awesome Words", "PWA") {}
+
+  string baseURI;
+  string public baseExtension = ".json";
+  uint256 public cost = 0.05 ether;
+  uint256 public maxSupply = 10000;
+  uint256 public maxMintAmount = 20;
+  bool public paused = false;
+  bool public revealed = false;
+  string public notRevealedUri;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI,
+    string memory _initNotRevealedUri
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
+    setNotRevealedURI(_initNotRevealedUri);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
 
   // public
-  function mint() public payable {
+  function mint(uint256 _mintAmount) public payable {
     uint256 supply = totalSupply();
-    require(supply + 1 <= 1000);
-    
-    Word memory newWord = Word(
-        string(abi.encodePacked('PWA #', uint256(supply + 1).toString())), 
-        "Pretty Awesome Words are all you need to feel good. These NFTs are there to inspire and uplift your spirit.",
-        randomNum(361, block.difficulty, supply).toString(),
-        randomNum(361, block.timestamp, supply).toString(),
-        wordsValues[randomNum(wordsValues.length, block.difficulty, supply)]);
-    
+    require(!paused);
+    require(_mintAmount > 0);
+    require(_mintAmount <= maxMintAmount);
+    require(supply + _mintAmount <= maxSupply);
+
     if (msg.sender != owner()) {
-      require(msg.value >= 0.005 ether);
+      require(msg.value >= cost * _mintAmount);
     }
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
     
-    words[supply + 1] = newWord;
-    _safeMint(msg.sender, supply + 1);
+    if(revealed == false) {
+        return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
   }
 
-  function randomNum(uint256 _mod, uint256 _seed, uint _salt) public view returns(uint256) {
-      uint256 num = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, _seed, _salt))) % _mod;
-      return num;
+  //only owner
+  function reveal() public onlyOwner {
+      revealed = true;
   }
   
-  function buildImage(uint256 _tokenId) public view returns(string memory) {
-      Word memory currentWord = words[_tokenId];
-      return Base64.encode(bytes(
-          abi.encodePacked(
-              '<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">',
-              '<rect height="500" width="500" fill="hsl(',currentWord.bgHue,', 50%, 25%)"/>',
-              '<text x="50%" y="50%" dominant-baseline="middle" fill="hsl(',currentWord.textHue,', 100%, 80%)" text-anchor="middle" font-size="41">',currentWord.value,'</text>',
-              '</svg>'
-          )
-      ));
+  function setCost(uint256 _newCost) public onlyOwner {
+    cost = _newCost;
+  }
+
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+    maxMintAmount = _newmaxMintAmount;
   }
   
-  function buildMetadata(uint256 _tokenId) public view returns(string memory) {
-      Word memory currentWord = words[_tokenId];
-      return string(abi.encodePacked(
-              'data:application/json;base64,', Base64.encode(bytes(abi.encodePacked(
-                          '{"name":"', 
-                          currentWord.name,
-                          '", "description":"', 
-                          currentWord.description,
-                          '", "image": "', 
-                          'data:image/svg+xml;base64,', 
-                          buildImage(_tokenId),
-                          '"}')))));
+  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+    notRevealedUri = _notRevealedURI;
   }
 
-  function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
-      require(_exists(_tokenId),"ERC721Metadata: URI query for nonexistent token");
-      return buildMetadata(_tokenId);
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
   }
 
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+ 
   function withdraw() public payable onlyOwner {
     // This will pay HashLips 5% of the initial sale.
     // You can remove this if you want, or keep it in to support HashLips and his channel.
     // =============================================================================
-    //(bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    //require(hs);
+    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
+    require(hs);
     // =============================================================================
     
     // This will payout the owner 95% of the contract balance.
